f00a71b85fc13153bf5db417ff68a4087e0e3319
PhanBaoThanh <thanhninjahero@gmail.com>
2022-10-12 16:37:04 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import video from './video/video.mp4'
+import {useState,useRef,useEffect} from 'react'
 
 function App() {
+  const [isPause,setIsPause] = useState(false)
+  const videoRef = useRef()
+  useEffect(() => {
+    if(isPause)
+      videoRef.current.pause()
+    else
+      videoRef.current.play()
+  },[isPause])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <video ref={videoRef} autoPlay muted loop className='video'>
+        <source src={video} type='video/mp4' />
+      </video>
+      <div className='box'>
+        <h1 className='header'>Video Project</h1>
+      </div>
+
+      <div className='button' onClick={() => setIsPause(!isPause)}>
+        <p>Play</p>
+        <p>Pause</p>
+        <div className={`btn ${isPause ? 'right' : ''}`}></div>
+      </div>
     </div>
   );
 }
